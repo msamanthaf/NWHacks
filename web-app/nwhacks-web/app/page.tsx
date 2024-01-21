fa99e4bc413c6849ee791ddbe6d064a8d84d7807
draihan <draihan@student.ubc.ca>
2024-01-20 20:37:15 -0800

    get sdk and locations

--- web-app/nwhacks-web/app/page.tsx
+++ web-app/nwhacks-web/app/page.tsx
@@ -1,113 +1,158 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import {
+  MappedinMap,
+  TGetVenueMakerOptions,
+  TMapViewOptions,
+} from "@mappedin/mappedin-js";
+import "@mappedin/mappedin-js/lib/mappedin.css";
+import { useEffect, useMemo, useState } from "react";
+import useMapChanged from "../hooks/useMapChanged";
+import useMapView from "../hooks/useMapView";
+import useVenueMaker from "../hooks/useVenueMaker";
+import "./globals.css";
+import test from "node:test";
+
+/* This demo shows you how to draw a path between two locations. */
+export default function NavigationExample() {
+  const credentials = useMemo<TGetVenueMakerOptions>(
+    () => ({
+      mapId: "659efcf1040fcba69696e7b6",
+      key: "65a0422df128bbf7c7072349",
+      secret:
+        "5f72653eba818842c16c4fdb9c874ae02100ffced413f638b7bd9c65fd5b92a4",
+
+      // map
+      // mapId: "65ac4f1dca641a9a1399dc38",
+      // key: "65ac681dca641a9a1399dc54",
+      // secret:
+      //   "0fec37ff6e01a712698b75c2b2bd6bd2cee01a6ab890e7eb5a7610d17163c6fb",
+    }),
+    []
+  );
+  const venue = useVenueMaker(credentials);
+
+  const mapOptions = useMemo<TMapViewOptions>(
+    () => ({
+      xRayPath: true, // X Ray enables seeing the path through walls
+      multiBufferRendering: true, // Multi buffer rendering is necessary for features like x ray
+    }),
+    []
+  );
+  const { elementRef, mapView } = useMapView(venue, mapOptions);
+  // console.log("🚀 ~ NavigationExample ~ venue:", venue?.locations);
+ 
+
+  let locationsID: any = [];
+  venue?.locations.map((location) => {
+    locationsID.push(location.id);
+  });
+
+  console.log(locationsID);
+
+  /* Start navigation when the map loads */
+  useEffect(() => {
+    if (!mapView || !venue) {
+      return;
+    }
+
+    /*
+     * All maps made in Maker will contain a location called "footprintcomponent"
+     * which represents the exterior "footprint"
+     * You can use this location to get the nearest entrance or exit
+     */
+    const startLocation = venue.locations.find((location) =>
+      location.id.includes("footprintcomponent")
+    );
+    // Navigate to some location on another floor
+    const endLocation = venue.locations.find((location) =>
+      location.id.includes("safewalk")
+    );
+
+    if (startLocation && endLocation) {
+      // Generate a route between these two locations
+      const directions = startLocation.directionsTo(endLocation);
+      if (directions && directions.path.length > 0) {
+        // The Journey class draws the path & can be configured with a few options
+        mapView.Journey.draw(directions, {
+          polygonHighlightColor: "#e74c3c", // Start and end polygons colour
+          departureMarkerTemplate: (props) => {
+            // The departure marker is the person at the start location
+            return `<div style="display: flex; flex-direction: column; justify-items: center; align-items: center;">
+            <div class="departure-marker">${
+              props.location ? props.location.name : "Departure"
+            }</div>
+            ${props.icon}
+            </div>`;
+          },
+          destinationMarkerTemplate: (props) => {
+            // The destination marker is the pin at the end location
+            return `<div style="display: flex; flex-direction: column; justify-items: center; align-items: center;">
+            <div class="destination-marker">${
+              props.location ? props.location.name : "Destination"
+            }</div>
+            ${props.icon}
+            </div>`;
+          },
+          connectionTemplate: (props) => {
+            // The connection marker is the button to switch floors on the map
+            return `<div class="connection-marker">Take ${props.type} ${props.icon}</div>`;
+          },
+          pathOptions: {
+            nearRadius: 0.25, // The path size in metres at the nearest zoom
+            farRadius: 1, // The path size in metres at the furthest zoom
+            color: "#40A9FF", // Path colour
+            displayArrowsOnPath: false, // Arrow animation on path
+            showPulse: true, // Pulse animation on path
+            pulseIterations: Infinity, // How many times to play the pulse animation
+          },
+        });
+
+        // Set the map (floor level) to start at the beginning of the path
+        mapView.setMap(directions.path[0].map);
+      }
+    }
+    // Update the selected map state
+    setSelectedMap(mapView.currentMap);
+  }, [mapView, venue]);
+
+  // Track the selected map with state, for the UI
+  const [selectedMap, setSelectedMap] = useState<MappedinMap | undefined>();
+
+  /* Monitor floor level changes and update the UI */
+  useMapChanged(mapView, (map) => {
+    setSelectedMap(map);
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div id="app">
+      <div id="ui">
+        {venue?.venue.name ?? "Loading..."}
+        {venue && selectedMap && (
+          <select
+            value={selectedMap.id}
+            onChange={(e) => {
+              if (!mapView || !venue) {
+                return;
+              }
+
+              const floor = venue.maps.find((map) => map.id === e.target.value);
+              if (floor) {
+                mapView.setMap(floor);
+              }
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            {venue?.maps.map((level, index) => {
+              return (
+                <option value={level.id} key={index}>
+                  {level.name}
+                </option>
+              );
+            })}
+          </select>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <div id="map-container" ref={elementRef}></div>
+    </div>
   );
 }
