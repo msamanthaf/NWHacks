961ddd105dc032a0208f9b76ee663c1601162157
Flayre <mariesamantha.f@gmail.com>
2024-01-21 10:14:46 -0800

    Added speech to text

--- web-app/nwhacks-web/app/mappedIn/page.tsx
+++ web-app/nwhacks-web/app/mappedIn/page.tsx
@@ -1,153 +1,157 @@
 "use client";
-
+import React, { useEffect, useMemo, useState } from "react";
 import {
   MappedinMap,
   TGetVenueMakerOptions,
   TMapViewOptions,
 } from "@mappedin/mappedin-js";
+import { TextField, Button } from "@mui/material";
 import "@mappedin/mappedin-js/lib/mappedin.css";
-import { useEffect, useMemo, useState } from "react";
 import useMapChanged from "@/hooks/useMapChanged";
 import useMapView from "@/hooks/useMapView";
 import useVenueMaker from "@/hooks/useVenueMaker";
 import "@/app/globals.css";
-
 import getLocation from "../getLocation";
+import MicIcon from "@mui/icons-material/Mic";
 
-/* This demo shows you how to draw a path between two locations. */
 export default function Map() {
+  const [spokenText, setSpokenText] = useState("");
+  const [isListening, setIsListening] = useState(false);
+
+  const startSpeechRecognition = () => {
+    if ("webkitSpeechRecognition" in window) {
+      const recognition = new window.webkitSpeechRecognition();
+
+      recognition.onstart = () => {
+        setIsListening(true);
+      };
+
+      recognition.onend = () => {
+        setIsListening(false);
+      };
+
+      recognition.onresult = (event) => {
+        const spokenResult = event.results[0][0].transcript;
+        setSpokenText(spokenResult);
+      };
+
+      recognition.onerror = (event) => {
+        console.error("Speech recognition error:", event.error);
+      };
+
+      recognition.start();
+    } else {
+      console.error("Speech recognition not supported in this browser");
+    }
+  };
+
   const credentials = useMemo<TGetVenueMakerOptions>(
     () => ({
       mapId: "65acc6a1ca641a9a1399dc75",
       key: "65ad46db83bd240b9aaf48bb",
-      secret:
-        "2eab22d952cdd43cf3edaa10bfa2fbd959508be823aabd8f491c006864b195e6",
-
-      // map ams full
-      // mapId: "659efcf1040fcba69696e7b6",
-      // key: "65a0422df128bbf7c7072349",
-      // secret:
-      //   "5f72653eba818842c16c4fdb9c874ae02100ffced413f638b7bd9c65fd5b92a4",
-
-      // map
-      // mapId: "65ac4f1dca641a9a1399dc38",
-      // key: "65ac681dca641a9a1399dc54",
-      // secret:
-      //   "0fec37ff6e01a712698b75c2b2bd6bd2cee01a6ab890e7eb5a7610d17163c6fb",
+      secret: "2eab22d952cdd43cf3edaa10bfa2fbd959508be823aabd8f491c006864b195e6",
     }),
     []
   );
+
   const venue = useVenueMaker(credentials);
 
   const mapOptions = useMemo<TMapViewOptions>(
     () => ({
-      xRayPath: true, // X Ray enables seeing the path through walls
-      multiBufferRendering: true, // Multi buffer rendering is necessary for features like x ray
+      xRayPath: true,
+      multiBufferRendering: true,
     }),
     []
   );
+
   const { elementRef, mapView } = useMapView(venue, mapOptions);
-  // console.log("🚀 ~ NavigationExample ~ venue:", venue?.locations);
+  const [selectedMap, setSelectedMap] = useState<MappedinMap | undefined>();
 
-  // let locationsID: any = [];
-  // venue?.locations.map((location) => {
-  //   locationsID.push(location.id);
-  // });
-
-  // console.log(locationsID);
-
-  /* Start navigation when the map loads */
   useEffect(() => {
     if (!mapView || !venue) {
       return;
     }
 
-    // get location from text
     let locationArray: any = [];
 
-    getLocation(
-      "I am in front of perugia right now, where can i find the nearest fountain?"
-    ).then((data) => {
+    getLocation(spokenText).then((data) => {
       locationArray = data;
 
-      // console.log(locationArray[0]);
-
-      /*
-       * All maps made in Maker will contain a location called "footprintcomponent"
-       * which represents the exterior "footprint"
-       * You can use this location to get the nearest entrance or exit
-       */
       const startLocation = venue.locations.find((location) =>
         location.id.includes(locationArray[0])
       );
-      // Navigate to some location on another floor
+
       const endLocation = venue.locations.find((location) =>
         location.id.includes(locationArray[1])
       );
 
       if (startLocation && endLocation) {
-        // Generate a route between these two locations
         const directions = startLocation.directionsTo(endLocation);
         if (directions && directions.path.length > 0) {
-          // The Journey class draws the path & can be configured with a few options
           mapView.Journey.draw(directions, {
-            polygonHighlightColor: "#e74c3c", // Start and end polygons colour
+            polygonHighlightColor: "#e74c3c",
             departureMarkerTemplate: (props) => {
-              // The departure marker is the person at the start location
               return `<div style="display: flex; flex-direction: column; justify-items: center; align-items: center;">
-            <div class="departure-marker">${
-              props.location ? props.location.name : "Departure"
-            }</div>
-            ${props.icon}
-            </div>`;
+                <div class="departure-marker">${
+                  props.location ? props.location.name : "Departure"
+                }</div>
+                ${props.icon}
+              </div>`;
             },
             destinationMarkerTemplate: (props) => {
-              // The destination marker is the pin at the end location
               return `<div style="display: flex; flex-direction: column; justify-items: center; align-items: center;">
-            <div class="destination-marker">${
-              props.location ? props.location.name : "Destination"
-            }</div>
-            ${props.icon}
-            </div>`;
+                <div class="destination-marker">${
+                  props.location ? props.location.name : "Destination"
+                }</div>
+                ${props.icon}
+              </div>`;
             },
             connectionTemplate: (props) => {
-              // The connection marker is the button to switch floors on the map
               return `<div class="connection-marker">Take ${props.type} ${props.icon}</div>`;
             },
             pathOptions: {
-              nearRadius: 0.25, // The path size in metres at the nearest zoom
-              farRadius: 1, // The path size in metres at the furthest zoom
-              color: "#40A9FF", // Path colour
-              displayArrowsOnPath: false, // Arrow animation on path
-              showPulse: true, // Pulse animation on path
-              pulseIterations: Infinity, // How many times to play the pulse animation
+              nearRadius: 0.25,
+              farRadius: 1,
+              color: "#40A9FF",
+              displayArrowsOnPath: false,
+              showPulse: true,
+              pulseIterations: Infinity,
             },
           });
 
-          // Set the map (floor level) to start at the beginning of the path
           mapView.setMap(directions.path[0].map);
         }
       }
-      // Update the selected map state
+
       setSelectedMap(mapView.currentMap);
     });
-  }, [mapView, venue]);
+  }, [mapView, venue, spokenText]);
 
-  // Track the selected map with state, for the UI
-  const [selectedMap, setSelectedMap] = useState<MappedinMap | undefined>();
-
-  /* Monitor floor level changes and update the UI */
   useMapChanged(mapView, (map) => {
     setSelectedMap(map);
   });
 
   return (
-    <div id="app">
-      <div id="ui">
-        {venue?.venue.name ?? "Loading..."}
-        {venue && selectedMap && (
-          <>
-            {/* Flooor */}
+    <div className="max-h-screen max-w-screen">
+      <div id="app">
+	  <TextField
+          label="Tell the AI which venue is nearby and your destination"
+          variant="outlined"
+          value={spokenText}
+          fullWidth
+          onChange={(e) => setSpokenText(e.target.value)}
+        />
+        <Button
+          variant="outlined"
+          onClick={startSpeechRecognition}
+          startIcon={<MicIcon />}
+          disabled={isListening}
+        >
+          {isListening ? "Listening..." : "Start Listening"}
+        </Button>
+        <div id="ui">
+          {venue?.venue.name ?? "Loading..."}
+          {venue && selectedMap && (
             <select
               value={selectedMap.id}
               onChange={(e) => {
@@ -163,19 +167,16 @@ export default function Map() {
                 }
               }}
             >
-              {venue?.maps.map((level, index) => {
-                return (
-                  <option value={level.id} key={index}>
-                    {level.name}
-                  </option>
-                );
-              })}
+              {venue?.maps.map((level, index) => (
+                <option value={level.id} key={index}>
+                  {level.name}
+                </option>
+              ))}
             </select>
-          </>
-        )}
+          )}
+        </div>
+        <div id="map-container" ref={elementRef}></div>
       </div>
-
-      <div id="map-container" ref={elementRef}></div>
     </div>
   );
 }
